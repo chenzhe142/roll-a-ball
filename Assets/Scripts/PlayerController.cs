1b60b4f199a336881ba2133b307f4f357f521b19
chenzhe142 <Chenzhe@chenzhedeMBP.att.net>
2015-01-18 23:28:14 -0600

    Jan.18

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+	}
+
+	//	control physic attribution
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+		rigidbody.AddForce (movement * speed *Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);		
+			count = count + 1;
+			SetCountText ();
+		} 
+	}
+
+	void SetCountText()
+	{
+		winText.text = "";
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "YOU WIN!";		
+		}
+	}
+}
